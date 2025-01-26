11e5f62a4a2ab67584d06ab6462c2c8af59549a9
VMekh8 <mekhvitaliy88@gmail.com>
2025-01-26 20:37:36 +0200

    feat(building blocks): add query handler shared interface

---
+++ src/BuildingBlocks/BuildingBlock/CQRS/QueryHandlers/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlock.CQRS.Queries;
+using MediatR;
+
+namespace BuildingBlock.CQRS.QueryHandlers;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull;
