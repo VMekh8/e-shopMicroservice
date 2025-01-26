1b7f1da22c5cb73d7a6fa1c56d504e35186c4efa
VMekh8 <mekhvitaliy88@gmail.com>
2025-01-26 20:22:12 +0200

    feat(building blocks): create command shared interface

---
+++ src/BuildingBlocks/BuildingBlock/CQRS/Queries/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{}
+
+public interface ICommand : ICommand<Unit> {}
