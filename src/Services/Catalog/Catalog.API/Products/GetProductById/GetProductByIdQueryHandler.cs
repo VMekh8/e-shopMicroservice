1304ef463966167d3d2182e6a911cd61ccadef68
VMekh8 <mekhvitaliy88@gmail.com>
2025-01-28 17:38:31 +0200

    feat(productById): create GetProductById query
    - added product not found exception

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using BuildingBlock.CQRS.Queries;
+using BuildingBlock.CQRS.QueryHandlers;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Mapster;
+using Marten;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResponse>;
+
+public record GetProductByIdResponse(Product product);
+
+internal sealed class GetProductByIdQueryHandler : IQueryHandler<GetProductByIdQuery, GetProductByIdResponse>
+{
+    private readonly IQuerySession _session;
+    private readonly ILogger<GetProductByIdQueryHandler> _logger;
+
+    public GetProductByIdQueryHandler(IQuerySession session, ILogger<GetProductByIdQueryHandler> logger)
+    {
+        _session = session;
+        _logger = logger;
+    }
+
+    public async Task<GetProductByIdResponse> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("GetProductByIdQuery.Handler called by query: {query}", query);
+
+        var product = await _session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResponse(product);
+    }
+}
