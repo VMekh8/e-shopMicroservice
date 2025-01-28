5f24efd4c2beed72311165aeaf06bcf2a2affec1
VMekh8 <mekhvitaliy88@gmail.com>
2025-01-28 18:32:51 +0200

    feat(GetProduct): add get product by category endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryEndPoint.cs
@@ -0,0 +1,21 @@
+﻿using Carter;
+using MediatR;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public class GetProductsByCategoryEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+           var result = await sender.Send(new GetProductByCategoryQuery(category));
+           return Results.Ok(result);
+
+        }).WithName("GetProductsByCategory")
+        .WithDescription("GetProductsByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products By Category");
+    }
+}
