0625f7ddc137f01aababa0e42ab830ab044b84b5
VMekh8 <mekhvitaliy88@gmail.com>
2025-01-30 14:16:39 +0200

    feat(validate): add ExceptionHandler for Catalog.API

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,9 @@ using BuildingBlock.BehaviorPipeline;
 using Carter;
 using FluentValidation;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
@@ -22,6 +25,33 @@ builder.Services.AddMarten(opt =>
 builder.Services.AddCarter();
 
 var app = builder.Build();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception is null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredKeyedService<ILogger<Program>>();
+
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.MapCarter();
 
