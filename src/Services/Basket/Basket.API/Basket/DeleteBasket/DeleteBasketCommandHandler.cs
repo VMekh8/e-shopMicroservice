a03192723d252c19d4959647907ec5faa1c50d1c
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 00:49:35 +0200

    feat(deleteBasket): add delete basket command handler

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Basket.API.Data;
+using BuildingBlock.CQRS.Queries;
+using BuildingBlock.CQRS.QueryHandlers;
+
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsDeleted);
+
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    private readonly IBasketRepository _repository;
+
+    public DeleteBasketCommandHandler(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+        var result = await _repository.DeleteBasket(request.UserName, cancellationToken);
+        return new DeleteBasketResult(true);
+    }
+}
