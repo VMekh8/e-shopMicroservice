ca76b060a695df69a2523356a9dcfa33afa56662
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 00:16:18 +0200

    feat(db): register martenDb config in basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,13 @@
+using Basket.API.Models;
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
