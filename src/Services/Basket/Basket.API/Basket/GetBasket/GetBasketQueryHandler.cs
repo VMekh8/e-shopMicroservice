3d98b19ca5a90b4c1184bc8eaeab2eb6ec1e7650
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 00:19:25 +0200

    feat(getBasket): create get basket query handler

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Basket.API.Data;
+using Basket.API.Models;
+using BuildingBlock.CQRS.Queries;
+using BuildingBlock.CQRS.QueryHandlers;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketQueryResult>;
+
+public record GetBasketQueryResult(ShoppingCart Cart);
+
+internal sealed class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketQueryResult>
+{
+    private readonly IBasketRepository _repository;
+
+    public GetBasketQueryHandler(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetBasketQueryResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _repository.GetBasket(request.UserName, cancellationToken);
+
+        return new GetBasketQueryResult(result);
+    }
+}
