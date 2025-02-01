f567c8c85d6af9a50bbac41dd616d0b463eea438
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 00:33:20 +0200

    feat(basket): add store basket command handler

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Basket.API.Data;
+using Basket.API.Models;
+using BuildingBlock.CQRS.Queries;
+using BuildingBlock.CQRS.QueryHandlers;
+
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    private readonly IBasketRepository _repository;
+
+    public StoreBasketCommandHandler(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _repository.StoreBasket(command.Cart, cancellationToken);
+        return new StoreBasketResult(command.Cart.UserName);
+    }
+}
