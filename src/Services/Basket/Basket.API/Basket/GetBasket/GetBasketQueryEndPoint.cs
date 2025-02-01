2d1fdb487bf4c2cc03eff46941469e9a225b81ce
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 00:24:48 +0200

    feat(basket): create Get Basket endpoint

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryEndPoint.cs
@@ -0,0 +1,27 @@
+﻿using Basket.API.Models;
+using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketQueryEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{username}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName));
+
+            var response = result.Adapt<GetBasketResponse>();
+            return Results.Ok(response);
+
+        }).WithName("GetBasket")
+        .WithDescription("GetBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Basket by username");
+    }
+}
