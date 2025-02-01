80f92869ea90743847dc5dceb30f81a131c933b8
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 00:01:12 +0200

    feat(repository): create basket repository
    - adedd carter, mapster, marten libs

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using Basket.API.Models;
+using BuildingBlock.Exceptions;
+using Marten;
+
+namespace Basket.API.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDocumentSession _session;
+
+    public BasketRepository(IDocumentSession session)
+    {
+        _session = session;
+    }
+
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellation = default)
+    {
+        var basket = await _session.LoadAsync<ShoppingCart>(userName, cancellation);
+        return basket ?? throw new NotFoundException();
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellation = default)
+    {
+        _session.Store(cart);
+        await _session.SaveChangesAsync(cancellation);
+        return cart;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellation = default)
+    {
+        _session.Delete<ShoppingCart>(userName);
+        await _session.SaveChangesAsync(cancellation);
+        return true;
+    }
+}
