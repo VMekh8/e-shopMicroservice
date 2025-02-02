df8740a37504a3b67b0d6182aab1e3533ed74133
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-02 14:54:56 +0200

    feat(setup): registerd mediatR, carter, fluenvalidation

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,24 @@
 using Basket.API.Data;
 using Basket.API.Models;
+using BuildingBlock.BehaviorPipeline;
+using Carter;
+using FluentValidation;
 using Marten;
 
+var assembly = typeof(Program).Assembly;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddCarter();
+
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddMarten(options =>
 {
@@ -13,5 +29,7 @@ builder.Services.AddMarten(options =>
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
