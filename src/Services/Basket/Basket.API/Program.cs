8b6116a7040762760e8b1b8204a2e25d9bd47d68
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-04 16:37:11 +0200

    feat(basket): add healthcheks for cache and db in basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -4,11 +4,16 @@ using BuildingBlock.BehaviorPipeline;
 using BuildingBlock.Exceptions.Handlers;
 using Carter;
 using FluentValidation;
+using HealthChecks.UI.Client;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var assembly = typeof(Program).Assembly;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var dbConnectionString = builder.Configuration.GetConnectionString("Database");
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
 
 builder.Services.AddMediatR(config =>
 {
@@ -23,14 +28,14 @@ builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddMarten(options =>
 {
-    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Connection(dbConnectionString!);
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
 builder.Services.AddStackExchangeRedisCache(opt =>
 {
-    opt.Configuration = builder.Configuration.GetConnectionString("Redis");
-
+    opt.Configuration = redisConnectionString;
+    opt.InstanceName = "Basket";
 });
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
@@ -38,10 +43,20 @@ builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(dbConnectionString!)
+    .AddRedis(redisConnectionString!);
+
 var app = builder.Build();
 
 app.UseExceptionHandler(opt => { });
 
 app.MapCarter();
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
