e1268095e76a55166e27c425f3b102f007349e64
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-06 03:16:49 +0200

    feat(db): create database
    - Installed EF Core
    - Configured DbContext
    - Configured SQLite
    - Created Migration

---
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -0,0 +1,17 @@
+﻿using Discount.gRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
+{
+    public DbSet<Coupon> Coupons { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "Apple iPhone 15", Description = "Iphone 15 discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung Galaxy S23", Description = "Samsung discount", Amount = 100 }
+        );
+    }
+}
--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,14 +1,21 @@
-
-
+using Discount.gRPC.Data;
 using Discount.gRPC.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DiscountContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Database")!);
+});
 
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-app.MapGrpcService<DiscountService>();
+app.UseMigration();
+
+app.MapGrpcService<DiscountService>();  
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
