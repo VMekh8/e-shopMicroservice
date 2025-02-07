e677df6e7fc439d6f59a6f57f404dc95ce257ac3
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-08 01:37:31 +0200

    feat(basket): added a gRPC client for store basket handler method

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -2,6 +2,7 @@
 using Basket.API.Models;
 using BuildingBlock.CQRS.Queries;
 using BuildingBlock.CQRS.QueryHandlers;
+using Discount.gRPC;
 
 namespace Basket.API.Basket.StoreBasket;
 
@@ -12,15 +13,32 @@ public record StoreBasketResult(string UserName);
 public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     private readonly IBasketRepository _repository;
+    private readonly DiscountProtoService.DiscountProtoServiceClient _discountProto;
 
-    public StoreBasketCommandHandler(IBasketRepository repository)
+    public StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
     {
         _repository = repository;
+        _discountProto = discountProto;
     }
 
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        await DeductDiscount(command, cancellationToken);
+
         var result = await _repository.StoreBasket(command.Cart, cancellationToken);
-        return new StoreBasketResult(command.Cart.UserName);
+        return new StoreBasketResult(command.Cart.UserName); 
     }
+
+    private async Task DeductDiscount(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        foreach (var item in command.Cart.Items)
+        {
+            var coupon = await _discountProto.GetDiscountAsync(
+                new GetDiscountRequest { ProductName = item.ProductName },
+                cancellationToken: cancellationToken
+            );
+
+            item.Price -= coupon.Amount;
+        }
+    } 
 }
