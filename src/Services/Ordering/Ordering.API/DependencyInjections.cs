e6d7851c5a35dba3db86e4cf5f1fc882f1ae14ec
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-12 01:07:22 +0200

    feat(DI): registered layers dependencies

---
+++ src/Services/Ordering/Ordering.API/DependencyInjections.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+
+namespace Ordering.Application;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddCarter();
+
+        return services;
+    }
+
+    public static WebApplication UseWebServices(this WebApplication app)
+    {
+        app.MapCarter();
+
+        return app;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjections.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<,>(opt =>
+        {
+            opt.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+}
