a4af3b3de68addf1c59f79019126face14f64cf5
VMekh8 <mekhvitaliy88@gmail.com>
2025-02-12 13:30:13 +0200

    feat: add logic for product model in domain

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,10 +1,26 @@
 ﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Domain.Models;
 
-public class Product : Entity<Guid>
+public class Product : Entity<ProductId>
 {
     public string Name { get; private set; } = string.Empty;
 
     public decimal Price { get; private set; } = decimal.Zero;
+
+    public static Product Create(ProductId id,string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Price = price
+        };
+
+        return product;
+    }
 }
