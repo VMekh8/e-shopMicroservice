042a27c81bb1ae6346a14a128f715101b0deac74
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-05 22:49:11 +0200

    feat: create pagination request and result for get orders

---
+++ src/BuildingBlocks/BuildingBlock/Pagination/PaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingBlock.Pagination;
+
+public class PaginatedResult<TEntity>
+    (int pageIndex, int pageSize, long count, IEnumerable<TEntity> data) 
+        where TEntity : class
+{
+    public int PageSize { get; } = pageSize;
+    public int PageIndex { get; } = pageIndex;
+    public long Count { get; } = count;
+    public IEnumerable<TEntity> Data { get; } = data;
+}
