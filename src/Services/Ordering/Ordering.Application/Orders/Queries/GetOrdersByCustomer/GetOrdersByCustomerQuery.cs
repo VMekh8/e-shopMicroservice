789ad9772ad313fc548c7389ab6552a2f54a1b1e
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-05 22:39:27 +0200

    feat: added get orders by customer query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlock.CQRS.Queries;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomersQueryResult>;
+
+public record GetOrdersByCustomersQueryResult(IEnumerable<OrderDto> Orders);
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlock.CQRS.QueryHandlers;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Extensions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerQueryHandler : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomersQueryResult>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetOrdersByCustomerQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetOrdersByCustomersQueryResult> Handle(GetOrdersByCustomerQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await _context.Orders
+            .Include(o => o.OrderItems)
+            .Where(o => o.CustomerId == CustomerId.Of(request.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomersQueryResult(orders.ToOrderDtoList());
+    }
+}
