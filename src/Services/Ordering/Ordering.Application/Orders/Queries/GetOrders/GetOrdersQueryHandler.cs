cafb9d4323a086048c823149dcae049171db705f
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-05 22:56:29 +0200

    feat: added get orders query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using BuildingBlock.CQRS.QueryHandlers;
+using BuildingBlock.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Dtos;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersQueryHandler : IQueryHandler<GetOrdersQuery, GetOrdersQueryResult>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetOrdersQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetOrdersQueryResult> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+    {
+        var pageSize = request.Request.PageSize;
+        var pageIndex = request.Request.PageIndex;
+
+        var totalOrdersCount = await _context.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await _context.Orders
+            .Include(o => o.OrderItems)
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersQueryResult(
+            new PaginatedResult<OrderDto>(
+                pageIndex,
+                pageSize,
+                totalOrdersCount,
+                orders.ToOrderDtoList())
+        );
+    }
+}
