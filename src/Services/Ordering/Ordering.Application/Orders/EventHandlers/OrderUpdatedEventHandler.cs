1d5a3a0e8a72304b6c6fb7ca707169819287e6a6
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-05 21:25:04 +0200

    feat: create orders event handler templates

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderUpdatedEventHandler : INotificationHandler<OrderUpdatedEvent>
+{
+    private readonly ILogger<OrderUpdatedEventHandler> _logger;
+
+    public OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain event handled: {domainEvent}" + notification.GetType());
+        return Task.CompletedTask;
+    }
+}
