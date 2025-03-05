414221c5ec9d6bfa44a8ea0b16412820b38df58f
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-05 22:26:23 +0200

    feat: added a get orders by name query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlock.CQRS.Queries;
+using Ordering.Application.Dtos;
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public record GetOrdersByNameQuery(string OrderName) : IQuery<GetOrdersByNameQueryResult>;
+
+public record GetOrdersByNameQueryResult(IEnumerable<OrderDto> Orders);
