78df3b91073185c0fc9839b7032b3cba2d6cbe19
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-14 20:36:23 +0200

    feat: add get orders by name endpoint

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByNameEndPoint.cs
@@ -0,0 +1,28 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersByNameRequest(string OrderName);
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByNameEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (GetOrdersByNameRequest request, ISender sender) =>
+            {
+                var query = new GetOrdersByNameQuery(request.OrderName);
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetOrdersByNameResponse>();
+
+                return Results.Ok(response);
+
+            }).WithName("Get Orders")
+            .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Orders");
+    }
+}
