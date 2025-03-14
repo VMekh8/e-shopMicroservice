2edfa8ad572ed528fb67c7f5626c26d24751ca63
VMekh8 <mekhvitaliy88@gmail.com>
2025-03-14 21:34:06 +0200

    feat: add get orders query endpoint

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersEndPoint.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlock.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersRequest(PaginatedRequest GetOrdersPaginatedRequest);
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrdersEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] GetOrdersRequest request, ISender sender) =>
+            {
+                var query = new GetOrdersQuery(request.GetOrdersPaginatedRequest);
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetOrdersByNameResponse>();
+
+                return Results.Ok(response);
+
+            }).WithName("GetOrders")
+            .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get all orders");
+    }
+}
